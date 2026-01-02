ae478deba95292a43962f175ed5fdc7ebe912234
unknown <1993bhargava@gmail.com>
2026-01-02 19:48:43 +0530

    javascript functions

---
+++ JS_Functions/index.js
@@ -0,0 +1,148 @@
+// functions:
+
+// syntax:
+
+// function function_name(){
+//     return;
+// }
+
+
+
+// var a=20;
+// var b=50;
+
+// var result = a+b;
+
+// console.log(result);
+
+// function addition(a=10,b=10) {
+//     console.log("a------",a);
+//     console.log("b--------",b)
+//     // var a = 50;
+//     // var b = 50;
+
+//     var result = a + b;
+
+//     // console.log("line 24---",result);
+//     return result;
+
+// }
+
+// console.log("line 28----",addition(70,50));
+
+// 0. normal functions
+// 1. arrow functions
+// 2. named functions
+// 3. name less function
+// 4. function expressions
+// 5. ifee (imedeately invoked functions)
+// 6. function curring
+
+// function addition(a,b){
+//     let result = a+b;
+//     return result;
+// }
+
+
+// arrow functions:
+
+// let addition = (a,b) => {
+//     let result = a+b;
+//     return result;
+// }
+
+// let addition = (a,b) => a+b;
+
+// this keyword
+
+// console.log("46-----",addition(10,20));
+
+// let sum = addition(20,30);
+
+// console.log("sum----",sum);
+
+
+// let array = [10,20,30,40,50];
+
+
+// ()=>{ }
+
+
+// array.map((ele)=>{
+//  console.log(ele);
+// });
+
+
+// {
+
+// }
+
+// if(){
+
+// }
+
+// loops 
+// {
+
+// }
+// for(){
+
+// }
+
+// function {
+
+// }
+
+// IIFE
+
+// let addition = (a,b) => {
+//     let result = a+b;
+//     return result;
+// }
+
+
+
+// (function(a,b){
+//     let result = a+b;
+//     console.log(result);
+// })(10,20);
+
+
+// ((a,b)=>{
+// let result = a+b;
+//     console.log(result);
+// })(10,20);
+
+
+function addtion(){
+    a+b
+}
+
+function mul(){
+A*B
+}
+
+function sub(){
+a-b
+}
+
+function devide(){
+a/b
+}
+
+addtion()
+sub()
+
+dispatchEvent()
+
+
+findWeek(){
+
+}
+
+2 - mondy
+3 - tues
+
+caluculator
+weeksName
+monthName
---
+++ JS_Functions/variable.js
@@ -0,0 +1,9 @@
+var boul = "rise";
+var cup = "carry";
+var bottle = "water";
+
+
+var - global scope
+let  - function scope / block
+const  - fuction scope / block
+
