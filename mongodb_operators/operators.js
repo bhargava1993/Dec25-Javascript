e1369e38733f40ee40f555a115d4022a84410363
unknown <1993bhargava@gmail.com>
2026-01-07 09:02:29 +0530

    operatores in mongodb

---
+++ mongodb_operators/operators.js
@@ -0,0 +1,34 @@
+db.users.insertMany([
+{ name: "Bala", age: 30, city: "Bangalore", role: "admin" },
+{ name: "Charan", age: 22, city: "Chennai", role: "user" },
+{ name: "Divya", age: 28, city: "Delhi", role: "user" },
+{ name: "Esha", age: 35, city: "Mumbai", role: "manager" }
+])
+
+"age": 30, "city": "Bangalore", "role": "admin"
+
+
+==  $eq
+
+>   $gt
+
+>=  $gte
+
+<   $lt
+ 
+<=   $lte
+
+!= $ne
+
+$in
+$nin
+
+
+AND   $and
+
+OR    $or
+
+NOT  $not
+
+NOR  $nor
+
